6f9e9becaed536722be36893a63a7d4b124df231
kanishka04sharma <kanishka04sharma@gmail.com>
2024-01-29 21:48:23 +0530

    today code

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./Navbar.css"
+//import datas from "../../utils/data.js"
+const Navbar = ({data}) => {
+  return (
+  <div className='container'>
+   {
+    data.map((data)=>(
+         <div className='card-container'>
+      <img src={data.img} ></img>
+      <div className='avatar'>
+        <img src={data.avatar}></img>
+      </div>
+      <div className='text'>
+        <p> {data.text}</p>
+      </div>
+    </div>
+    ))
+   }
+  </div>
+  
+  );
+}
+
+export default Navbar
---
+++ src/utils/Data.js
@@ -0,0 +1,36 @@
+const data=[
+    {
+        id:1,
+        img:"https://webdesignmastery.github.io/Travel_Hub_13-02-24/assets/customer-1.jpg",
+        avatar:"https://webdesignmastery.github.io/Travel_Hub_13-02-24/assets/customer-user-1.jpg",
+        text:"The app made booking my trip a breeze—effortless and efficient!"
+
+    },
+
+    {
+        id:2,
+        img:"https://webdesignmastery.github.io/Travel_Hub_13-02-24/assets/customer-2.jpg",
+        avatar:"https://webdesignmastery.github.io/Travel_Hub_13-02-24/assets/customer-user-2.jpg",
+        text:"Incredible customer service—prompt responses and problem-solving"
+    },
+    {
+        id:3,
+        img:"https://webdesignmastery.github.io/Travel_Hub_13-02-24/assets/customer-3.jpg",
+        avatar:"https://webdesignmastery.github.io/Travel_Hub_13-02-24/assets/customer-user-3.jpg",
+        text:" Love the personalized itineraries, keeping me organized throughout trip."
+    },
+    {
+        id:4,
+        img:"https://webdesignmastery.github.io/Travel_Hub_13-02-24/assets/customer-4.jpg",
+        avatar:"https://webdesignmastery.github.io/Travel_Hub_13-02-24/assets/customer-user-4.jpg",
+        text:"The travel updates feature kept me informed,especially during uncertain times."
+    },
+    {
+         id:5,
+        img:"https://webdesignmastery.github.io/Travel_Hub_13-02-24/assets/customer-1.jpg",
+        avatar:"https://webdesignmastery.github.io/Travel_Hub_13-02-24/assets/customer-user-1.jpg",
+        text:"The app made booking my trip a breeze—effortless and efficient!"
+    }
+
+];
+export default data;
